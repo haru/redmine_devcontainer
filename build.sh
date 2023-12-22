e096eb91ff96b5d983a7386e1f7ca459381fdb62
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2023-12-22 17:25:19 +0900

    version args

--- build.sh
+++ build.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
+if [ -z "$1" ]
+then
+    echo "Usage: build.sh REDMINE_VERSION [RUBY_VERSION]" >&2
+    exit 1
+fi
 
-docker buildx build -t haru/redmine_devcontainer . --platform linux/amd64,linux/arm64/v8 \
+REDMINE=$1
+
+RUBY=3.2
+if [ -n "$2" ]
+then
+  RUBY=$2
+fi
+
+CONTAINER_VERSION="${REDMINE}-ruby${RUBY}"
+docker buildx build -t haru/redmine_devcontainer:${CONTAINER_VERSION} . \
+    --build-arg RUBY=$RUBY \
+    --build-arg REDMINE_VERSION=$REDMINE \
+    --platform linux/amd64,linux/arm64/v8 \
     --output=type=image,push=true
