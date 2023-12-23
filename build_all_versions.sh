5585f66d5f5be67c6d24fdc3a9dd776479b75356
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2023-12-24 08:58:07 +0900

    change suffix of config

--- build_all_versions.sh
+++ build_all_versions.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd `dirname $0`
 
-grep -v '^\s*#' supported_versions.txt |grep -v '^\s*$' | xargs -r -I arg echo sh -x build.sh arg | sh -
+grep -v '^\s*#' supported_versions.conf |grep -v '^\s*$' | xargs -r -I arg echo sh -x build.sh arg | sh -
